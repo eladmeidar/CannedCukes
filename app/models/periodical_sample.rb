a90c368fc1aaa4e409cc079a77e204c1d6c85516
Elad Meidar <elad@eizesus.com>
2010-04-01 11:10:45 -0400

    About to make something stupid

--- app/models/periodical_sample.rb
+++
@@ -1,6 +0,0 @@
-class PeriodicalSample < ActiveRecord::Base
-  
-  validates_presence_of :count
-  
-  belongs_to :market_symbol
-end
--- app/models/stocktwit_user.rb
+++
@@ -1,3 +0,0 @@
-class StocktwitUser < Stocktwits::GenericUser
-  set_table_name 'stocktwits_users'
-end
