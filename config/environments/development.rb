a4694df37e8f71c940f024dd41eecb22aec53979
Elad Meidar <elad@eizesus.com>
2010-04-02 21:28:17 -0400

    README

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,3 +18,5 @@ config.action_mailer.raise_delivery_errors = false
 config.action_mailer.default_url_options = {:host => "example.com"}
 
 config.gem 'restful_authentication'
+config.gem 'will_paginate'
+config.gem 'searchlogic'
