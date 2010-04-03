40b60c3c3f1c85cd0f1d6328b13787f23b69fb10
Elad Meidar <elad@eizesus.com>
2010-04-02 21:43:47 -0400

    Fixed registration emails

--- app/models/user_mailer.rb
+++ app/models/user_mailer.rb
@@ -3,14 +3,14 @@ class UserMailer < ActionMailer::Base
     setup_email(user)
     @subject    += 'Please activate your new account'
   
-    @body[:url]  = "http://YOURSITE/activate/#{user.activation_code}"
+    @body[:url]  = "http://cannedcukes.heroku.com/activate/#{user.activation_code}"
   
   end
   
   def activation(user)
     setup_email(user)
     @subject    += 'Your account has been activated!'
-    @body[:url]  = "http://YOURSITE/"
+    @body[:url]  = "http://cannedcukes.heroku.com/"
   end
   
   # TODO: Fix ugly link, you can use route helpers
@@ -23,8 +23,8 @@ class UserMailer < ActionMailer::Base
   protected
     def setup_email(user)
       @recipients  = "#{user.email}"
-      @from        = "ADMINEMAIL"
-      @subject     = "[YOURSITE] "
+      @from        = "CannedCukes"
+      @subject     = "CannedCukes: "
       @sent_on     = Time.now
       @body[:user] = user
     end
